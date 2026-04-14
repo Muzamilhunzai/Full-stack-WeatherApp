0aae34371772444f32504a10df2dff7583647184
Muzamil hussain <muzamilhussain55441@gmail.com>
2026-04-14 16:16:17 +0500

    CHANGES

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/weatherapp", );
+    await mongoose.connect("mongodb://localhost:27017/weather", );
     console.log("Database connected");
   } catch (error) {
     console.log(error);
